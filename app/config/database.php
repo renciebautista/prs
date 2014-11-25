ddb5380c10bebf32dfbe31c4a54c0ebae1e55525
unknown <rencie.bautista@yahoo.com>
2014-11-25 10:04:02 +0800

    update production settings

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'chaset7_prs',
+			'username'  => 'chaset7_prs',
+			'password'  => 'prs031988',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
